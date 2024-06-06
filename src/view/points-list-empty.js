a400fa130b549777815e625dde65bae13ae0a078
Takiera <yana.250296@mail.ru>
2024-06-06 12:59:27 +0300

    Добавлен интерфейс страницы при отсутствии точек в маршруте

---
+++ src/view/points-list-empty.js
@@ -0,0 +1,18 @@
+import AbstractView from '../framework/view/abstract-view';
+import { FilterEmptyMessages } from '../const';
+
+const createEpmtyListTemplate = (filter) => `<p class="trip-events__msg">${FilterEmptyMessages[filter]}</p>`;
+
+export default class PointsListEmptyView extends AbstractView {
+  #filter = null;
+
+  constructor ({ filter }) {
+    super();
+    this.#filter = filter;
+  }
+
+  get template() {
+    return createEpmtyListTemplate(this.#filter);
+  }
+
+}
